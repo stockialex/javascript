ae4a5be889a1369615ac4a147f4f6048fa7e3954
Alex <stocki.ladislao@gmail.com>
2021-04-22 14:58:46 -0300

    update quote-generator

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -3,17 +3,32 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 // let apiQuotes = [];
 
+// show is loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// the quote is loaded 
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // // Get quotes from API
 // async function getQuotes(){
+//     loading();
 //     const APIURL = 'https://type.fit/api/quotes';
 
 //     try{ 
 //         const response = await fetch(APIURL);
 //         apiQuotes = await response.json();
 //         pickQuote();
+//         complete();
 //     } catch(error){
 //         // catch error
 //     }
@@ -21,6 +36,7 @@ const newQuoteBtn = document.getElementById("new-quote");
 
 // pick a random quote from local source
 function pickQuote(){
+    complete();
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     authorText.textContent = quote.author;
     quoteText.textContent = quote.text;
